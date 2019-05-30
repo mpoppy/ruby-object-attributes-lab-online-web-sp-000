d3ed570dacb8deffb943ebdb05d32c2f44e29ca1
Marc Poppy <poppy.marc@gmail.com>
2019-05-30 05:12:35 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,9 @@
 
 class Person
-  def name=(name)
+  def name=(name) #setter method, this is the writer
     @name = name
   end
-  def name
+  def name #this is the reader method, the getter
     @name
   end
 end
